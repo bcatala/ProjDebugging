c83725f35d4030b4ac68a35bfa2494cfe0191b48
bcatala <bernat.catalai@students.salle.url.edu>
2019-06-23 17:22:27 +0200

    Fix-Avg hotel prices
    Fix the function that calculate the average of the prices

--- model/destination/destination.c
+++ model/destination/destination.c
@@ -46,10 +46,11 @@ double getAverageHotelPrice(Destination dest) {
 	double average = 0.0;
 	int i;
 
-	for (i = 1; i < dest.nHotels; i++) {
-		average += *(dest.hotelPrices + i) / dest.nHotels;
+	for (i = 0; i < dest.nHotels; i++) {
+		average += dest.hotelPrices[i] ;
 	}
-
+	
+	average=average/ dest.nHotels;
 	return average;
 }
 
