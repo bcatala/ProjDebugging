76914e862bf1971a655924ee26c937931e973845
bcatala <bernat.catalai@students.salle.url.edu>
2019-07-13 12:16:19 +0200

    FIX-Add printf error
    Add printf error to show when there is an error

--- logic/functions/functions.c
+++ logic/functions/functions.c
@@ -11,6 +11,9 @@ void showAllDestinations(List *l) {
 		printSortMenu();
 
 		option = askUserForOption();
+		if (option < 1 || option > MAX_SORTING) {
+            printOptionError();
+        }
 	} while (option < 1 || option > MAX_SORTING);
 
 	option -= 1;
