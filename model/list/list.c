fc38041c11d7b552db5b581695b231a4e4e50eee
bcatala <bernat.catalai@students.salle.url.edu>
2019-07-14 18:43:51 +0200

    FIX-Free destinacio
    Free memoery from list destinacio

--- model/list/list.c
+++ model/list/list.c
@@ -94,7 +94,9 @@ void removePoi(List * l, int mode) {
 	// Comprovem que no estigui buida ni estiguem a un fantasma
 	if (!isEmpty(*l) && l -> poi[mode] != l -> last && l -> poi[mode] != l -> first) {
 		//Desem la posicio del node en memòria a un punter auxiliar, per no perdre'l
+		
 		Node * aux = l -> poi[mode];
+		freeAll(l->poi[mode]->dest);
 		int i;
 		//Deslliguem el node de la resta
 		for (i = 0; i < MAX_SORTING; i++) {
